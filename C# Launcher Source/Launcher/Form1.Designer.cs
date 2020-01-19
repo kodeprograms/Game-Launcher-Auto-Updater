3c47f84ff05775028674a44dfd03820b21e760f4
BoysWhoCryPrograms <kodyvoyles97@gmail.com>
2020-01-18 16:16:02 -0800

    Initial Release

---
+++ C# Launcher Source/Launcher/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FloralWhite;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(634, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 404);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(616, 18);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(425, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(299, 305);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.Location = new System.Drawing.Point(12, 425);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ready";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label2.Location = new System.Drawing.Point(700, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "X";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseEnter += new System.EventHandler(this.label2_MouseEnter);
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 445);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ C# Launcher Source/Launcher/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Threading;
+
+namespace Launcher
+{
+    public partial class Form1 : Form
+    {
+        private bool _dragging = false;
+        private Point _offset;
+        private Point _start_point = new Point(0, 0);
+        public string Verison;
+        public string CurrentVerison;
+        public int VerisonNumber;
+        public int CurrentVerisonNumber;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;
+            _start_point = new Point(e.X, e.Y);
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this._start_point.X, p.Y - this._start_point.Y);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)//When Form first loads up
+        {
+            WebClient client1 = new WebClient();
+            client1.DownloadFile("https://dl.dropboxusercontent.com/s/3iv3coq6eaty9lf/news.txt?dl=0.html", "Common/n102s.file");
+            client1.DownloadFile("https://dl.dropboxusercontent.com/s/d0tdyrs7uydoaia/BackImage.jpg?dl=0", "Common/Core/LauncherImg.bmp");
+            using (System.IO.StreamReader file = new System.IO.StreamReader("Common/n102s.file"))
+            {
+                string News = file.ReadToEnd();
+                richTextBox1.Text = News;
+            }
+            this.BackgroundImage = new Bitmap("Common/Core/LauncherImg.bmp");
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            MethodInvoker Max = delegate
+            {
+                try
+                {
+                    progressBar1.Maximum = (int)e.TotalBytesToReceive / 100;
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker Value = delegate
+            {
+                try
+                {
+                    progressBar1.Value = (int)e.BytesReceived / 100;
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker Labelhelp = delegate
+            {
+                try
+                {
+                    label1.Text = (int)e.BytesReceived / 1000 + " KB/" + (int)e.TotalBytesToReceive / 1000 + " KB";
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+
+
+            //Show download Progress
+            progressBar1.Invoke(Max);
+            progressBar1.Invoke(Value);
+            label1.Invoke(Labelhelp);
+            
+        }
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MethodInvoker DownloadComplete = delegate
+            {
+                try
+                {
+                    label1.Text = "Download Complete.";
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker ExtractingFiles = delegate
+            {
+                try
+                {
+                    label1.Text = "Extracting Files.";
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker DoneExtractingFiles = delegate
+            {
+                try
+                {
+                    label1.Text = "Done Extracting Files.";
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker Ready = delegate
+            {
+                try
+                {
+                    label1.Text = "Ready to Play.";
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker Upgrade = delegate
+            {
+                try
+                {
+                    progressBar1.Value += 20;
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker Done = delegate
+            {
+                try
+                {
+                    progressBar1.Value = 100;
+                    progressBar1.Maximum = 100;
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker Play = delegate
+            {
+                try
+                {
+                    button1.Text = "Play";
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+
+
+            //File done downloading, extract the file.
+            label1.Invoke(DownloadComplete);
+            label1.Invoke(ExtractingFiles);
+            progressBar1.Invoke(Upgrade);
+            //Uncomment this if you want to delete directories
+            //Directory.Delete("Common/Core/CHeroes_Data", true);
+            File.Delete("Common/Core/Program.exe");
+            try
+            {
+                ZipFile.ExtractToDirectory("Update.zip", "Common/Core");
+            }
+            catch {};
+            File.Delete("Update.zip");
+            label1.Invoke(DoneExtractingFiles);
+            progressBar1.Invoke(Done);
+            button1.Invoke(Play);
+            label1.Invoke(Ready);
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("Common/v102s.file"))
+            {
+                using (System.IO.StreamReader file = new System.IO.StreamReader("Common/v102s.file"))
+                {
+                    CurrentVerison = file.ReadLine();
+                    CurrentVerisonNumber = Convert.ToInt32(CurrentVerison);
+                }
+                if (button1.Text == "Check")
+                {
+                    //Start Update Check
+                    label1.Text = "Starting Check";
+                    WebClient client2 = new WebClient();
+                    client2.DownloadFile("https://dl.dropboxusercontent.com/s/vkves45qoyptroc/version.txt?dl=0.html", "Common/v102s.file");
+                    using (System.IO.StreamReader file = new System.IO.StreamReader("Common/v102s.file"))
+                    {
+                        Verison = file.ReadLine();
+                        VerisonNumber = Convert.ToInt32(Verison);
+                        label1.Text = "Version Found";
+                    }
+                    Thread th = new Thread(Updater);
+                    th.IsBackground = true;
+                    th.Start();
+                }
+                else if (button1.Text == "Play")
+                {
+                    Process.Start(@"Common\Core\Program.exe");
+                    this.Close();
+                }
+            }
+            else 
+            {
+                CurrentVerisonNumber = 0;
+            }
+        }
+
+
+        public void Updater() //Thread to make Updater Run smoothly
+        {
+            MethodInvoker Updating = delegate
+            {
+                try
+                {
+                    label1.Text = "Update Found. Downloading Now.";
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker Play = delegate
+            {
+                try
+                {
+                    button1.Text = "Play";
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker NoUpdate = delegate
+            {
+                try
+                {
+                    label1.Text = "No Update Found.";
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker LatestVersion = delegate
+            {
+                try
+                {
+                    label1.Text = "Your Files are Up to Date.";
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+            MethodInvoker Done = delegate
+            {
+                try
+                {
+                    progressBar1.Value = 100;
+                    progressBar1.Maximum = 100;
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+
+
+
+            if (VerisonNumber > CurrentVerisonNumber)
+            {
+                //Download Current Update from DropBox zip file!
+                label1.Invoke(Updating);
+                WebClient client3 = new WebClient();
+                client3.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                client3.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                client3.DownloadFileAsync(new Uri("https://dl.dropboxusercontent.com/s/kgxvwe48ddo2fwx/Update.zip?dl=0"), "Update.zip");
+            }
+            else if (VerisonNumber == CurrentVerisonNumber)
+            {
+                //No update start game!
+                label1.Invoke(NoUpdate);
+                progressBar1.Invoke(Done);
+                button1.Invoke(Play);
+                label1.Invoke(LatestVersion);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure that you want to quit?", "Are you sure?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                DialogResult dialogResult1 = MessageBox.Show("Really?", "Double Checking.", MessageBoxButtons.YesNo);
+                if (dialogResult1 == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+                else if (dialogResult1 == DialogResult.Yes){ }
+            }
+            else if (dialogResult == DialogResult.No) { }
+        }
+
+        private void label2_MouseEnter(object sender, EventArgs e)
+        {
+            label2.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            label2.ForeColor = System.Drawing.Color.White;
+        }
+    }
+}
